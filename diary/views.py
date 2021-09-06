a7fdfe235b6d2ca6a77b23393abc0018d84ce3dc
rika25fgfg <rika25greeeen777@gmail.com>
2021-09-06 12:13:53 +0900

    初期セットアップ(テンプレート登録)

---
+++ diary/views.py
@@ -0,0 +1,9 @@
+from django.http.response import Http404, HttpResponse
+from django.shortcuts import render
+from django.views import generic
+# Create your views here.
+def index(request):
+    return render(request, 'diary/index.html')
+
+class IndexView(generic.TemplateView):
+    template_name="diary/index.html"
