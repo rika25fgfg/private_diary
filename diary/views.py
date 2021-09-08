262bf36a9dcb7825000ba878fd6e4cbb12ff465f
rika25fgfg <rika25greeeen777@gmail.com>
2021-09-08 12:23:59 +0900

    フォーム機能

--- diary/views.py
+++ diary/views.py
@@ -1,7 +1,12 @@
+import logging
+from django.urls import reverse_lazy
 from django.shortcuts import render
 from django.views import generic
 from .forms import InquiryForm
 # Create your views here.
+
+logger = logging.getLogger(__name__)
+
 def index(request):
     return render(request, 'diary/index.html')
  
@@ -11,3 +16,9 @@ class IndexView(generic.TemplateView):
 class InquiryView(generic.FormView):
     template_name = "diary/inquiry.html"
     form_class = InquiryForm
+    success_url = reverse_lazy('diary:inquiry')
+
+    def form_valid(self, form):
+        form.send_email()
+        logger.info('Inquiry sent by {}'.format(form.cleaned_data['name']))
+        return super().form_valid(form)
